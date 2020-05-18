5720c1577659537e74e74abc7848a5a2fcd517f2
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-05-18 23:07:20 +0530

    Complete Calculator Project
    Complete Calculator Project

--- 02 Calculator/home.js
+++ 02 Calculator/home.js
@@ -1,16 +1,16 @@
 function ButtonPress(val)
 {
-    let v = document.getElementById("value").value
+    let v = document.getElementById("value3").value
     strlen=v.length
     if(strlen!=0)
     {
         val=v+val
-        document.getElementById("value").value=val
+        document.getElementById("value3").value=val
     }
     else
     {
-        document.getElementById("value").value=""
-        document.getElementById("value").value=val
+        document.getElementById("value3").value=""
+        document.getElementById("value3").value=val
     }
 
 }
@@ -24,17 +24,64 @@ let Num1=Num2=null
 
 function Plus()
 {
-    Num1 = Number(document.getElementById("value").value)
-    document.getElementById("value").value=""
+    Num1 = Number(document.getElementById("value3").value)
+    document.getElementById("value0").value=Num1
+    document.getElementById("value1").value="+"
+    document.getElementById("value2").value=""
+    document.getElementById("value3").value=""
+}
+function Minus()
+{
+    Num1 = Number(document.getElementById("value3").value)
+    document.getElementById("value0").value=Num1
+    document.getElementById("value1").value="-"
+    document.getElementById("value2").value=""
+    document.getElementById("value3").value=""
+}
+function Multiply()
+{
+    Num1 = Number(document.getElementById("value3").value)
+    document.getElementById("value0").value=Num1
+    document.getElementById("value1").value="*"
+    document.getElementById("value2").value=""
+    document.getElementById("value3").value=""
+}
+function Divide()
+{
+    Num1 = Number(document.getElementById("value3").value)
+    document.getElementById("value0").value=Num1
+    document.getElementById("value1").value="/"
+    document.getElementById("value2").value=""
+    document.getElementById("value3").value=""
+}
+
+function Power()
+{
+    Num1 = Number(document.getElementById("value3").value)
+    document.getElementById("value2").value=Num1
+    document.getElementById("value3").value=Num1*Num1
 }
 
 function Equal()
 {   
-    Num2 = Number(document.getElementById("value").value)
-    document.getElementById("value").value=Num1+Num2
+    Num2 = Number(document.getElementById("value3").value)
+    document.getElementById("value2").value=Num2
+    sign = document.getElementById("value1").value
+    if(sign==="+")
+    {
+        document.getElementById("value3").value=Num1+Num2
+    }
+    else if(sign==="-")
+    {
+        document.getElementById("value3").value=Num1-Num2
+    }
+    else if(sign==="*")
+    {
+        document.getElementById("value3").value=Num1*Num2
+    }
+    else if(sign==="/")
+    {
+        document.getElementById("value3").value=Num1/Num2
+    }
 }
 
-function Minus()
-{
-    Plus()
-}
